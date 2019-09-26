d46a4b2ea6e130198534ca3fe36b66e5b890551b
dongpengfei <dongpengfei826153155@gamil.com>
2019-09-26 15:31:37 +0800

    v1.0版本，可以直接使用版

---
+++ runnerjson.py
@@ -0,0 +1,35 @@
+import datetime
+from base import Base
+from writerjson import WriterJson
+from createjson import CreateJson
+
+jsondata = Base().toJson(Base().jsonDatafile())
+
+class RunnerJson(CreateJson):
+
+    def runnerTest(self,floor:int):
+        '''
+        1.获取数据源 √
+        2.获取json全路径 √
+        3.获取yaml参数 √
+        4.组装
+        5.输出到jsoncase/xxxx.txt
+        '''
+        nums = Base().getCaseElementNum()
+        jsondata = Base().toJson(Base().jsonDatafile())
+        for i in range(nums):
+            # 固定循环方式组成固定用例 与初始化迭代器指针对应
+            [self.cycleCase.__next__() for _ in range(i+1)]
+            newjson = self.createjson(jsondata,floor)
+            WriterJson(str(newjson))()
+            while True:#初始化迭代器指针
+                if self.cycleCase.__next__() == Base().readYamlcaseElement()[nums-1] :
+                    break
+
+if __name__ == '__main__':
+    print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),'正在执行')
+
+    R = RunnerJson()
+    [R.runnerTest(i) for i in range(1,5)] #生成case
+
+    print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),'执行完成')
